b1992cb13ac54a441a44f4678fcc01b555bea787
lucadard <lucadardenne@hotmail.com>
2023-05-24 16:13:33 -0300

    add api url as env variable

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { ApiNote, Category, Note } from './types'
 
-axios.defaults.baseURL = 'http://localhost:3000/'
+axios.defaults.baseURL = import.meta.env.VITE_API_URL
 
 function noteParser (noteData: ApiNote): Note {
   return {
