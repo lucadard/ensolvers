a4c7bc6716461e4db776bea928805b6d85055591
lucadard <lucadardenne@hotmail.com>
2023-05-24 15:29:36 -0300

    minor style fix

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -2,7 +2,7 @@ const Button = (props: any) => {
   return (
     <button
       {...props}
-      className='rounded-sm px-8 py-2 capitalize hover:bg-black/10'
+      className='rounded-full border px-4 py-2 capitalize hover:bg-black/10 md:px-8'
     >{props.children}
     </button>
   )
