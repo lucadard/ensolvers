44024f53db566417988f2bbf520e8380125f8746
lucadard <lucadardenne@hotmail.com>
2023-05-23 22:19:46 -0300

    implement note filtering & remove loading states

--- frontend/src/components/Notes.tsx
+++ frontend/src/components/Notes.tsx
@@ -2,45 +2,62 @@
 import { useState } from 'react'
 import { createPortal } from 'react-dom'
 import Modal from './Modal'
-import { Note } from '../types'
+import { Category, Note } from '../types'
 import { editNote, removeNote } from '../api'
 import { Link } from 'wouter'
 import Button from './Button'
 
-export function NoteList ({ notes, onNoteUpdate }: { notes: Note[], onNoteUpdate: (id: string, data: Partial<Note>, opt?: { delete: boolean }) => void }) {
+export function NoteList ({ notes, categories, onNoteUpdate }: { notes: Note[], categories: Category[], onNoteUpdate: (id: string, data: Partial<Note>, opt?: { delete: boolean }) => void }) {
+  const [categoryFilter, setCategoryFilter] = useState<Category['id']>(0)
+
   if (notes.length === 0) return <div>There's nothing to show here...</div>
 
+  const filteredNotes = notes
+    .filter(note => !categoryFilter
+      ? true
+      : note?.categories.some(cat => cat?.id === categoryFilter))
+
   return (
-    <ul className='grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-5 '>
-      {notes.map(note => <NoteCard key={note.id} data={note} onUpdate={onNoteUpdate} />)}
-    </ul>
+    <>
+      <select
+        defaultValue='all'
+        onChange={(e) => setCategoryFilter(+e.target.value)}
+        className='mb-5 w-full max-w-xs px-2 py-1'
+      >
+        <option value={0}>All</option>
+        {categories.map(category =>
+          <option
+            key={category.id}
+            value={category.id}
+          >{category.name}
+          </option>)}
+      </select>
+      <ul className='grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-5 '>
+        {filteredNotes.map(note => <NoteCard key={note.id} data={note} onUpdate={onNoteUpdate} />)}
+      </ul>
+    </>
   )
 }
 
 function NoteCard ({ data, onUpdate }: { data: Note, onUpdate: (id: string, data: Partial<Note>, opt?: { delete: boolean }) => void }) {
-  const [isLoading, setIsLoading] = useState(false)
   const [modal, setModal] = useState<React.ReactNode>(null)
 
   async function handleChangeState () {
-    setIsLoading(true)
     try {
       const editedNote = await editNote(data.id, { archieved: !data.archieved })
       onUpdate(data.id, editedNote)
     } catch (err) {
       console.error(err)
     }
-    setIsLoading(false)
   }
 
   async function handleRemove () {
-    setIsLoading(true)
     try {
       await removeNote(data.id)
       onUpdate(data.id, {}, { delete: true })
     } catch (err) {
       console.error(err)
     }
-    setIsLoading(false)
     handleHideModal()
   }
 
@@ -49,12 +66,10 @@ function NoteCard ({ data, onUpdate }: { data: Note, onUpdate: (id: string, data
       return setModal(
         <div className='p-4'>
           <p className='mb-4 text-xl'>Are you sure you want to delete this note?</p>
-          {!isLoading
-            ? <div className='flex gap-10'>
-              <Button onClick={handleRemove}>Yes</Button>
-              <Button onClick={handleHideModal}>No</Button>
-            </div>
-            : <p>Loading...</p>}
+          <div className='flex gap-10'>
+            <Button onClick={handleRemove}>Yes</Button>
+            <Button onClick={handleHideModal}>No</Button>
+          </div>
         </div>)
     }
   }
@@ -80,7 +95,7 @@ function NoteCard ({ data, onUpdate }: { data: Note, onUpdate: (id: string, data
         <p
           onClick={handleChangeState}
           className='cursor-pointer'
-        >{isLoading ? '◴' : data.archieved ? '⬆️' : '🗂️'}
+        >{data.archieved ? '⬆️' : '🗂️'}
         </p>
         <Link
           href={`/note/${data.id}`}
