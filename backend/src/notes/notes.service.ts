b7e4b5011d0d87e9dfcfd0466086bde2fbc58dfb
lucadard <lucadardenne@hotmail.com>
2023-05-23 22:17:43 -0300

    implement add & rmv categories relations in db

--- backend/src/notes/notes.service.ts
+++ backend/src/notes/notes.service.ts
@@ -12,16 +12,32 @@ export class NotesService {
         title: createNoteDto.title,
         content: createNoteDto.content,
         archieved: createNoteDto.archieved,
+        categories: {
+          create: createNoteDto.categories?.map((category) => ({
+            category: {
+              connectOrCreate: {
+                where: { name: category.name },
+                create: { name: category.name },
+              },
+            },
+          })),
+        },
       },
+      include: { categories: { select: { category: true } } },
     });
   }
 
   findAll() {
-    return this.prisma.note.findMany({});
+    return this.prisma.note.findMany({
+      include: { categories: { select: { category: true } } },
+    });
   }
 
   findOne(id: string) {
-    return this.prisma.note.findUnique({ where: { id } });
+    return this.prisma.note.findUnique({
+      where: { id },
+      include: { categories: true },
+    });
   }
 
   update(id: string, updateNoteDto: UpdateNoteDto) {
@@ -32,6 +48,35 @@ export class NotesService {
         content: updateNoteDto.content,
         archieved: updateNoteDto.archieved,
       },
+      include: { categories: { select: { category: true } } },
+    });
+  }
+
+  addCategory(id: string, categoryId: number) {
+    return this.prisma.note.update({
+      where: { id },
+      data: {
+        categories: {
+          upsert: {
+            where: { noteId_categoryId: { categoryId, noteId: id } },
+            create: { categoryId: categoryId },
+            update: {},
+          },
+        },
+      },
+      include: { categories: { select: { category: true } } },
+    });
+  }
+
+  removeCategory(id: string, categoryId: number) {
+    return this.prisma.note.update({
+      where: { id },
+      data: {
+        categories: {
+          delete: { noteId_categoryId: { categoryId, noteId: id } },
+        },
+      },
+      include: { categories: { select: { category: true } } },
     });
   }
 
